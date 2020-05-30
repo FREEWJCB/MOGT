30a864a15ab8045429cb1958838f001146f6a35a
Miguel Gizzi <36783490+migi-sama@users.noreply.github.com>
2020-05-30 03:22:36 -0400

    Actualizada migración empleado

--- database/migrations/2020_05_29_085858_create_empleados_table.php
+++ database/migrations/2020_05_29_085858_create_empleados_table.php
@@ -15,6 +15,14 @@ class CreateEmpleadosTable extends Migration
     {
         Schema::create('empleados', function (Blueprint $table) {
             $table->id();
+            $table->string('cedula',20);
+            $table->string('nombres',50);
+            $table->string('apellidos',50);
+            $table->char('sexo',9);
+            $table->string('telefono',50);
+            $table->string('correo',50);
+            $table->unsignedBigInteger('cargo_id');
+            $table->foreign('cargo_id')->references('id')->on('cargos');
             $table->timestamps();
         });
     }
