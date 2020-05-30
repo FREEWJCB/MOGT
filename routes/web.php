4672f74453877b3293bebf755e572ccedd189523
Miguel Gizzi <36783490+migi-sama@users.noreply.github.com>
2020-05-30 03:19:04 -0400

    Rutas Cargo y Empleado añadidas

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,7 @@ Route::resource('municipio','MunicipioController');
 Route::post('/parroquia/municipio','ParroquiaController@municipio');
 
 Route::resource('parroquia','ParroquiaController');
+
+Route::resource('cargo','CargoController');
+
+Route::resource('empleado','EmpleadoController');
