85bdf0f811de2102103de6804b4e86aa92915459
vojdel <vasquezpinedaj@gmail.com>
2020-06-22 00:41:24 -0400

    🚧 ADD: Se agregaron validadciones

--- resources/js/components/tipoDiscapacidad/ListComponent.vue
+++ resources/js/components/tipoDiscapacidad/ListComponent.vue
@@ -109,7 +109,7 @@
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">New Type Discapacity</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="clean">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
@@ -117,7 +117,10 @@
             <form>
               <div class="form-group">
                 <label for="recipient-name" class="col-form-label">Tipo de Discapacidad:</label>
-                <input type="text" class="form-control" id="recipient-name" v-model="tipoDiscapacidad.tipo_d">
+                <input type="text" class="form-control" id="recipient-name" name="recipient-name"
+                v-model="tipoDiscapacidad.tipo_d"
+                v-validate="'required|min:4|alpha_spaces'">
+                <span class="text-danger">{{ errors.first('recipient-name') }}</span>
               </div>
             </form>
           </div>
@@ -125,10 +128,12 @@
             <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="clean">Close</button>
             <button type="button" class="btn btn-primary" data-dismiss="modal"
             v-if="this.tipoDiscapacidad.id === 0"
-            @click="createTipoD">Save</button>
+            @click="createTipoD"
+            :disabled='errors.any() || !isComplete'>Save</button>
             <button type="button" class="btn btn-primary" data-dismiss="modal"
             v-else
-            @click="updateTipoD">Update</button>
+            @click="updateTipoD"
+            :disabled='errors.any() || !isComplete'>Update</button>
           </div>
         </div>
       </div>
@@ -151,6 +156,11 @@ export default {
         vista: 6, // resultados por ver
         paginas: [],
       }
+    }
+  },
+  computed: {
+    isComplete(){
+      return (this.tipoDiscapacidad.tipo_d == '')?false:true;
     }
   },
   methods: {
